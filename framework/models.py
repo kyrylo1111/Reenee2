bdec5ca115c5384c542b2fb0b2f293cf1666ae93
kyrylo1111 <96440229+kyrylo1111@users.noreply.github.com>
2022-01-15 20:14:46 +0200

    Add files via upload

---
+++ framework/models.py
@@ -0,0 +1,30 @@
+from app import db
+
+class Plant(db.Model):
+    __searchable__ = ['location', 'name', 'director_id']
+    __tablename__ = 'Plants'
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(64), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    director_id = db.Column(db.Integer, nullable=False)
+
+
+
+
+class Employee(db.Model):
+    __tablename__ = 'Employees'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    department_type = db.Column(db.Integer, nullable=False)
+    department_id = db.Column(db.Integer, nullable=False)
+
+
+class Salon(db.Model):
+    __tablename__ = 'Salons'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    department_type = db.Column(db.String(64), nullable=False)
+    department_id = db.Column(db.Integer, nullable=False)
+    salon = db.Column(db.String(64), nullable=False)
